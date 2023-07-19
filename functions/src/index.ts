628228a631b6b188935b4e0c4079f93724733abf
Chris Ozgo <chrisozgo99@gmail.com>
2023-07-18 20:03:17 -0400

    vm functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,17 +1,10 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
 import * as functions from "firebase-functions/v2";
 import * as admin from "firebase-admin";
 import cors from "cors";
 import { authenticate, linkedInSession } from "./helpers/scraping";
 import type { HttpsOptions } from "firebase-functions/v2/https";
+import { google } from "googleapis";
+import axios from "axios";
 
 admin.initializeApp();
 
@@ -50,3 +43,81 @@ export const auth = functions.https.onRequest(async (request, response) => {
     authenticate();
   });
 });
+
+export const isVMRunning = functions.https.onRequest(async (req, res) => {
+  corsHandler(req, res, async () => {
+    const auth = new google.auth.GoogleAuth({
+      scopes: ["https://www.googleapis.com/auth/compute"],
+    });
+
+    const compute = google.compute({
+      version: "v1",
+      auth,
+    });
+
+    const projectId = "introer-prod";
+    const zone = "us-east4-c";
+    const instanceName = "introer-instance-1";
+
+    try {
+      const response = await compute.instances.get({
+        project: projectId,
+        zone,
+        instance: instanceName,
+      });
+
+      const isRunning = response.data.status === "RUNNING";
+      res.status(200).send({ data: isRunning });
+    } catch (err) {
+      console.error(err);
+      res.status(500).send({ data: {
+        error: err,
+      },
+      });
+    }
+  });
+});
+
+export const runScriptOnVM = functions.https.onRequest(
+  async (req, res) => {
+    corsHandler(req, res, async () => {
+      const vmIp = "34.85.224.239:3000";
+
+      try {
+        const response = await axios.post(
+          `http://${vmIp}/run-script`,
+          req.body
+        );
+
+        return { data: response.data };
+      } catch (err) {
+        console.error(err);
+        return { data: {
+          error: err,
+        },
+        };
+      }
+    });
+  });
+
+export const passParams = functions.https.onCall(
+  async (request) => {
+    const vmIp = "34.85.224.34:3000";
+
+    try {
+      const response = await axios.post(
+        `http://${vmIp}/run-script`,
+        request.data,
+      );
+
+      console.log(response.data);
+
+      return { data: response.data };
+    } catch (err) {
+      console.error(err);
+      return { data: {
+        error: err,
+      },
+      };
+    }
+  });
